c47e931cfb0e8d98e5da5c9de2dcddb9e72d2ddc
letoast <ciroman@gmail.com>
2021-01-24 15:30:25 +0000

    trying preload from own server

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,43 @@
 <script lang="ts">
+  import ApolloClient, { gql } from "apollo-boost";
   import { onMount } from "svelte";
+
+const apiURL = "api";
+  const projectsQuery = gql`
+    query GetProjectsData {
+      projects {
+        title
+        content {
+          ... on ComponentSectionsRichText {
+            content
+          }
+          ... on ComponentSectionsImagesSlider {
+            images {
+              url
+            }
+          }
+          ... on ComponentSectionsLargeMedia {
+            media {
+              url
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  export async function preload({ params, query }) {
+    const client = new ApolloClient({
+      uri: "http://172.104.238.194:1337/graphql",
+      fetch: this.fetch,
+    });
+    const results = await client.query({
+      query: projectsQuery,
+      fetchPolicy: "no-cache",
+    });
+    return { projects: results.data.projects };
+  }
+
   export let date: date;
 
   onMount(async () => {
@@ -13,4 +51,8 @@
   <h1>Svelte + Node.js API</h1>
   <h2>The date according to Node.js is:</h2>
   <p>{date ? date : 'Loading date...'}</p>
+
+{#each projects as project}
+  <h1>{project.title}</h1>
+{/each}
 </main>
